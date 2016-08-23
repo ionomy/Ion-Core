94b5e4d3cf8418de8135cab90a7ae70d190a3901
io-eightyeight <io-eightyeight@users.noreply.github.com>
2016-08-23 09:23:47 +0000

    Fix fRelayTxes

--- src/main.cpp
+++ src/main.cpp
@@ -3575,6 +3575,8 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             vRecv >> pfrom->strSubVer;
         if (!vRecv.empty())
             vRecv >> pfrom->nStartingHeight;
+        if (!vRecv.empty())
+            pfrom->fRelayTxes = true;
 
         pfrom->cleanSubVer = SanitizeString(pfrom->strSubVer);
 
